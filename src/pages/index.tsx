a7deb94e94909aba55fb17703d5bdf3c7625de3a
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-07-07 12:31:03 -0300

    style: stylized list of posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
@@ -24,8 +25,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
 export default function Home({ postsPagination }: HomeProps) {
-  return <div>Oi</div>;
+  return (
+    <div className={styles.container}>
+      {[1, 2, 3, 4, 5].map(item => (
+        <div className={styles.containerPost}>
+          <h2>Como utilizar Hooks</h2>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.containerInfo}>
+            <span>15 Mar 2021</span>
+
+            <span>Joseph Oliveira</span>
+          </div>
+        </div>
+      ))}
+
+      {/* // eslint-disable-next-line react/button-has-type */}
+      <button className={styles.btnLoadMore} onClick={() => {}}>
+        Carregar mais posts
+      </button>
+    </div>
+  );
 }
 
 // export const getStaticProps = async () => {
