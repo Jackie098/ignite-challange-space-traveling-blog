d2a7c89a9de22a8f4aecb71d2bb8ebe6f3f9a2cc
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-07-20 16:07:02 -0300

    style: showing content of a post in screen and stylized the content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
+import { useRouter } from 'next/router';
 import styles from './header.module.scss';
 
 export default function Header() {
+  const { asPath } = useRouter();
+
+  // console.log(asPath);
+
+  const headerClass = asPath.includes('post')
+    ? styles.headerContainerInPost
+    : styles.headerContainer;
+
+  // console.log(styles.headerContainer);
+
   return (
-    <header className={styles.headerContainer}>
+    <header className={headerClass}>
       <img src="/images/logo.svg" alt="logo" />
     </header>
   );
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { RichText } from 'prismic-dom';
+import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -28,9 +30,56 @@ interface PostProps {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
 export default function Post({ post }: PostProps) {
-  console.log(post);
+  // console.log(post);
+  // console.log(post.data.content[0].body);
 
-  return <div>VamoVê</div>;
+  return (
+    <>
+      <Head>
+        <title>{post.data.title}</title>
+      </Head>
+
+      <div className={styles.bannerContainer}>
+        <img
+          className={styles.banner}
+          src={post.data.banner.url}
+          alt="Banner do texto"
+        />
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div className={styles.contentContainer}>
+          <header className={styles.header}>
+            <h1>{post.data.title}</h1>
+            <div className={styles.headerInformations}>
+              <span>{post.first_publication_date}</span>
+              <span>{post.data.author}</span>
+              <span>Tempo de leitura</span>
+            </div>
+          </header>
+
+          <main className={styles.content}>
+            {post.data.content.map(item => (
+              <div key={Math.random()}>
+                <h3 className={styles.blockTitle}>{item.heading}</h3>
+                <div
+                  className={styles.blockContent}
+                  dangerouslySetInnerHTML={{ __html: item.body }}
+                />
+              </div>
+            ))}
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -52,7 +101,23 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return {
     props: {
-      post: response,
+      post: {
+        uid: response.uid,
+        first_publication_date: response.first_publication_date,
+        data: {
+          author: response.data.author,
+          banner: {
+            url: response.data.banner.url,
+          },
+          content: response.data.content.map(item => {
+            return {
+              heading: item.header,
+              body: RichText.asHtml(item.body),
+            };
+          }),
+          title: response.data.title,
+        },
+      } as Post,
     },
   };
 };
